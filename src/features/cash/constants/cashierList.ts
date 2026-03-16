742ec02f883a4c8bc1c0d45dce7aacfbc5eba923
ebikhajeh <ebikhajeh@gmail.com>
2026-03-15 17:20:47 -0700

    ebi

--- src/features/cash/constants/cashierList.ts
+++ src/features/cash/constants/cashierList.ts
@@ -4,7 +4,7 @@ export type Cashier = {
 };
 
 export const CASHIER_LIST: Cashier[] = [
-  { id: "EBI-9650", label: "EBI - 9650" },
+  { id: "Ebi-9650", label: "Ebi - 9650" },
   { id: "SACHIN-2092", label: "Sachin - 2092" },
   { id: "HARVEY-1642", label: "Harvey - 1642" },
   { id: "KUSHAL-2900", label: "Kushal - 2900" },
